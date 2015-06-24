b8ec7cf70eb8bc7d422eee98a969e161addb9d05
Christian Melgarejo Bresanovich <christian.melgarejo@outlook.com>
2015-06-24 16:43:18 -0400

    Demo 0.3

--- lib/centralgps_webapp/utilities.ex
+++ lib/centralgps_webapp/utilities.ex
@@ -22,7 +22,7 @@ defmodule CentralGPSWebApp.Utilities do
   def centralgps_session(conn) do
     session = get_session(conn, :session_data)
     if (session != nil) do
-      IO.puts "session: #{inspect session}"
+      IO.puts "session: #{inspect session.auth_token}"
       conn = conn
         |> assign(:session, session)
         |> assign(:profile_image, if(session.profile_image != nil, do: session.profile_image, else: "images/profile/_placeholder.png"))
