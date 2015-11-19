1c256b70c6433d5ea836548350fa4ac65d66aa52
Christian Melgarejo Bresanovich <christian.melgarejo@outlook.com>
2015-11-19 10:19:31 -0300

    Done with account creation. TODO: account role assignations

--- web/controllers/client/security/account_controller.ex
+++ web/controllers/client/security/account_controller.ex
@@ -79,19 +79,23 @@ defmodule CentralGPSWebApp.Client.Security.AccountController do
   defp image_dir(client_id \\ ""), do: "images/client/#{client_id}/account"
   defp image_placeholder, do: String.replace(Enum.join([image_dir, centralgps_placeholder_file], "/"), "//", "/")
   defp api_method(account_type \\ "", action \\ "") when is_bitstring(action), do:
-    "/security/account/" <> account_type <> "/" <> action
+    String.replace(Enum.join(["/security/account/", account_type, action], "/"), "//", "/")
 
   defp list_records(s, p) do
     p = objectify_map(p)
-      |> (Map.update :offset, 1, &(parse_int(&1)))
-      |> (Map.update :limit, 10, &(parse_int(&1)))
-      |> (Map.update :search, nil, fn(v)->(v) end)
-      |> (Map.update :sort, nil, fn(v)->(v) end)
-      |> (Map.update :order, nil, fn(v)->(v) end)
-    qs = %{offset: p.offset, limit: p.limit,
-      search_column: "name",
-      search_phrase: p.search,
-      sort_column: p.sort, sort_order: p.order}
+      |> (Map.update :current, 1, &(parse_int(&1)))
+      |> (Map.update :rowCount, 10, &(parse_int(&1)))
+      |> (Map.update :searchColumn, nil, fn(v)->(v) end)
+      |> (Map.update :searchPhrase, nil, fn(v)->(v) end)
+      |> (Map.put :sort_column, nil)
+      |> (Map.put :sort_order, nil)
+    if Map.has_key?p, :sort do
+      p = Map.put(p, :sort_column, Map.keys(p.sort) |> hd)
+        |> Map.put(:sort_order, Map.values(p.sort) |> hd)
+    end
+    qs = %{offset: (p.current - 1) * p.rowCount, limit: p.rowCount,
+      search_column: p.searchColumn, search_phrase: p.searchPhrase,
+      sort_column: p.sort_column, sort_order: p.sort_order}
     {api_status, res} = api_get_json api_method, s.auth_token, s.account_type, qs
     rows = %{}
     if(api_status == :ok) do
@@ -99,7 +103,7 @@ defmodule CentralGPSWebApp.Client.Security.AccountController do
         rows = res.body.rows
           |> Enum.map(&(objectify_map &1))
           |> Enum.map &(%{id: &1.id, account_type: &1.account_type, name: &1.name,
-          identity_document: &1.identity_document, username: &1.username,
+          identity_document: &1.identity_document, login_name: &1.username,
           dob: &1.dob, emails: &1.emails, phones: &1.phones,
           timezone: &1.timezone, active: &1.active, blocked: &1.blocked,
           language_template: &1.language_template, image_path: &1.image_path,
@@ -122,14 +126,14 @@ defmodule CentralGPSWebApp.Client.Security.AccountController do
       if Map.has_key?(record, :res) do
         record = objectify_map(res.body.res)
         if res.body.status do
-          Map.merge %{status: res.body.status, msg: res.body.msg} , record
-          #%{id: record.id, name: record.name, identity_document: record.identity_document,
-          #username: record.username, dob: record.dob, emails: record.emails, phones: record.phones,
-          #timezone: record.timezone, active: record.active, blocked: record.blocked,
-          #language_template: record.language_template, image_path: record.image_path,
-          #activated_at: record.activated_at, deactivated_at: record.deactivated_at,
-          #created_at: record.created_at, updated_at: record.updated_at,
-          #xtra_info: record.xtra_info}
+          Map.merge %{status: res.body.status, msg: res.body.msg} , #record
+          %{id: record.id, account_type: record.account_type, name: record.name, identity_document: record.identity_document,
+          login_name: record.username, dob: record.dob, emails: record.emails, phones: record.phones,
+          timezone: record.timezone, active: record.active, blocked: record.blocked,
+          language_template: record.language_template, image_path: record.image_path,
+          activated_at: record.activated_at, deactivated_at: record.deactivated_at,
+          created_at: record.created_at, updated_at: record.updated_at,
+          xtra_info: record.xtra_info}
         end
       end
     else
@@ -156,49 +160,46 @@ defmodule CentralGPSWebApp.Client.Security.AccountController do
 
   defp save_record(s, p) do
     p = objectify_map(p)
+    language_template_id = 2 #TODO: remove this! :(
     if (!Map.has_key?p, :__form__), do: p = Map.put p, :__form__, :edit
     if (!Map.has_key?(p, :xtra_info) || p.xtra_info == ""), do: p = Map.put p, :xtra_info, nil
     if (!Map.has_key?(p, :account_type)), do: p = Map.put p, :account_type, "C"
     if (!Map.has_key?p, :image), do: p = Map.put(p, :image, nil), else:
       (if p.image == "", do: p = Map.put p, :image, nil) #if the parameter is there and it's empty, let's just NIL it :)
+    if (!Map.has_key?p, :active),
+      do: p = Map.put( p, :active, false),
+      else: p = Map.update(p, :active, false, &(&1 == "on"))
+    if (!Map.has_key?p, :blocked),
+      do: p = Map.put( p, :blocked, false),
+      else: p = Map.update(p, :blocked, false, &(&1 == "on"))
+    file = nil
+    if (p.image != nil) do #let's create a hash filename for the new pic.
+      image_path = (UUID.uuid4 <> "." <> (String.split(upload_file_name(p.image), ".") |> List.last)) |> String.replace "/", ""
+      {:ok, file} = File.read p.image.path
+      file = Base.url_encode64(file)
+    else #or take the already existing one
+      image_path = (String.split(p.image_path, image_dir(s.client_id)) |> List.last) |> String.replace "/", ""
+    end
     if (String.to_atom(p.__form__) ==  :edit) do
-      #image_filename = p.image_path
-      file = nil
-      if (p.image != nil) do #let's create a hash filename for the new pic.
-        image_path = (UUID.uuid4 <> "." <> (String.split(upload_file_name(p.image), ".") |> List.last)) |> String.replace "/", ""
-        {:ok, file} = File.read p.image.path
-        file = Base.url_encode64(file)
-      else #or take the already existing one
-        image_path = (String.split(p.image_path, image_dir(s.client_id)) |> List.last) |> String.replace "/", ""
-      end
-      data = %{ account_id: p.id, account_type: p.account_type, login_password: s.login_password, dob: p.dob, identity_document: p.identity_document,
+      data = %{ account_id: p.id, account_type: p.account_type, login_password: p.login_password, dob: p.dob, identity_document: p.identity_document,
         image_path: Enum.join([image_dir(s.client_id), image_path], "/"), image_bin: file,
-        info_emails: p.info_emails, info_phones: p.info_phones, language_template_id: p.language_template_id, name: p.name, info_emails: p.info_emails, xtra_info: p.xtra_info }
-      {api_status, res} = api_put_json api_method(data.account_type, data.id), s.auth_token, s.account_type, data
-      if api_status == :ok  do
-        if res.body.status && (p.image != nil) do #put the corresponding pic for the record.
-          dest_dir = Enum.join [Utilities.priv_static_path, image_dir(s.client_id)], "/"
-          File.rm Enum.join([dest_dir,  String.split(p.image_path, image_dir(s.client_id)) |> List.last], "/") #removes the old image
-          File.mkdir_p dest_dir
-          File.copy(p.image.path, Enum.join([dest_dir,  image_path], "/"), :infinity)
-        end
-      else
-        res = Map.put res, :body, %{ status: false, msg: res.reason }
+        info_emails: p.emails, info_phones: p.phones, language_template_id: language_template_id, name: p.name, timezone: p.timezone, xtra_info: p.xtra_info }
+      {api_status, res} = api_put_json api_method(data.account_type, data.account_id), s.auth_token, s.account_type, data
+    else
+      data = %{ client_id: s.client_id, account_type: p.account_type, login_name: p.login_name, login_password: p.login_password, dob: p.dob, identity_document: p.identity_document,
+        image_path: Enum.join([image_dir(s.client_id), image_path], "/"), image_bin: file,
+        info_emails: p.emails, info_phones: p.phones, language_template_id: language_template_id, name: p.name, timezone: p.timezone, xtra_info: p.xtra_info }
+      {api_status, res} = api_post_json api_method("create", s.account_type), s.auth_token, s.account_type, data
+    end
+    if api_status == :ok  do
+      if res.body.status && (p.image != nil) do #put the corresponding pic for the record.
+        dest_dir = Enum.join [priv_static_path, image_dir(s.client_id)], "/"
+        File.rm Enum.join([dest_dir,  String.split(image_path, image_dir(s.client_id)) |> List.last], "/") #removes the old image
+        File.mkdir_p dest_dir
+        File.copy(p.image.path, Enum.join([dest_dir, image_path], "/"), :infinity)
       end
     else
-      file = nil
-      if (p.image != nil) do
-        image_filename = (UUID.uuid4 <> "." <> (String.split(upload_file_name(p.image), ".") |> List.last)) |> String.replace "/", ""
-        {:ok, file} = File.read p.image.path
-        file = Base.url_encode64(file)
-      else
-        image_filename = image_placeholder
-      end
-      data = %{ account_type: p.account_type, configuration_id: s.client_id,
-        name: p.name, identity_document: p.identity_document, description: p.description,
-        lat: p.lat, lon: p.lon, image: image_filename, image_file: file,
-        detection_radius: p.detection_radius, xtra_info: p.xtra_info }
-      {_, res} = api_post_json api_method(p.account_type, "create"), s.auth_token, s.account_type, data
+      res = Map.put res, :body, %{ status: false, msg: res.reason }
     end
     res.body #let's return the message
   end
