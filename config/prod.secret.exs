76ab9660cbcb270dfdff5f88fb52fb3b063a6246
Christian Melgarejo Bresanovich <christian.melgarejo@outlook.com>
2016-01-19 08:31:02 -0300

    Added secret, becuse, why not? ffs

---
+++ config/prod.secret.exs
@@ -0,0 +1,7 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :centralgps_webapp, CentralgpsWebapp.Endpoint,
+  secret_key_base: "cR+TuOXuBMlx6KyijteJonTcNKWH3+QLVvArYIyRePcWVA2mYNiAXH7/TUSDlPg5"
